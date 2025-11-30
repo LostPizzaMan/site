e590af1ecac4f35196b12609bd50e07f779a3424
ItsNeil17 <neil@willofsteel.me>
2025-11-30 22:42:07 +0530

    feat: add drakon-guide redirect
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/drakon-guide',
+        destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default withMDX(config);
